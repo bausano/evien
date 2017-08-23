ebc39908c7065afcb2f00afa8d719900b11e12b3
bausano <gtasampserver@seznam.cz>
2017-08-23 20:27:58 +0100

    Preparing tests

---
+++ test/setup.spec.js
@@ -0,0 +1,17 @@
+const sinon = require('sinon')
+const chai = require('chai')
+const sinonChai = require('sinon-chai')
+
+process.env.PORT = 3000
+
+before(() => {
+  chai.use(sinonChai)
+})
+
+beforeEach(function beforeEach () {
+  this.sandbox = sinon.sandbox.create()
+})
+
+afterEach(function afterEach () {
+  this.sandbox.restore()
+})
